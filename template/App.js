128a23a57a8590685c92603c62e9d266ab90b396
abhinand v k <abhinandazokdio@gmail.com>
2022-10-22 23:19:37 +0530

    updated

--- template/App.js
+++ template/App.js
@@ -5,7 +5,23 @@ import FontRatio from './src/global_functions/font_ratio/FontRatio';
 import customColor from './src/theme/Color';
 import Fonts from './src/theme/Fonts';
 import ReactIcon from './src/assets/svg_icons/react.svg' 
+import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 const App = () => {
+  const progress = useSharedValue(1);
+
+  const rStyle =useAnimatedStyle(()=>{
+    return {
+      transform:[
+        {
+          scale:progress.value
+        }
+      ]
+    }
+  })
+
+  React.useEffect(()=>{
+    progress.value = withTiming(withSpring(2),1,true)
+  },[])
   return (
     <View style={styles.container}>
       <Text style={styles.textStyle}>WELCOME TO REACT NATIVE ATOM </Text>
